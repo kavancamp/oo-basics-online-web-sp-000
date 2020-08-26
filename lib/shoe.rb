6b24388da2842f20a808731db307e85884adb171
Keenah Vancampenhout <kvancamp@mail.com>
2020-08-26 23:18:49 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,16 @@
 # Make your shoe class here!
+class shoe
+  attr_accessor :color, :size, :material, :condition
+  
+  def initialize(brand)
+    @brand = brand
+  end
+  def brand
+    @brand
+  end 
+
+
+
+
+
+end 
