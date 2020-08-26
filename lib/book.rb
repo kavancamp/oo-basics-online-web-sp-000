103d029611fa2bcfc5badf19c446a585728b0aa3
Keenah Vancampenhout <kvancamp@mail.com>
2020-08-26 23:07:02 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -8,11 +8,19 @@ class Book
       @title
     end
 
-    def author=(author)
-      @author = author
-    end
-  def author
-    @author
-  end
+      def author=(author)
+        @author = author
+        end
 
+      def author
+        @author
+      end
+
+      def page_count=(num)
+        @page_count = num
+      end
+
+      def page_count
+        @page_count
+      end
 end
